c01aa4622159cea77a3e0286bbd3a3f08103bc04
Govind yadav <43997191+GtechGovind@users.noreply.github.com>
2022-03-11 12:08:29 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\Modules\Ticket\DashboardController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,6 @@ Route::post('/auth', [MainController::class, 'auth'])->name('auth');
 
 // PRODUCTS
 Route::get('/products', [ProductController::class, 'index'])->name('products');
+
+// TICKET
+Route::get('/ticket/dashboard', [DashboardController::class, 'index'])->name('ticket.dashboard');
