c7b149cfb38fedd44aba296002b5ee7f8ed63deb
Govind yadav <43997191+GtechGovind@users.noreply.github.com>
2022-03-11 13:36:20 +0530

    Create TicketViewController.php

---
+++ app/Http/Controllers/Modules/Ticket/TicketViewController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Modules\Ticket;
+
+use App\Http\Controllers\Controller;
+
+class TicketViewController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+}
