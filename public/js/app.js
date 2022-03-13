bcaffbd881740db375c45bb0a9f140078f35b51f
Govind yadav <43997191+GtechGovind@users.noreply.github.com>
2022-03-13 19:07:27 +0530

    s

--- public/js/app.js
+++ public/js/app.js
@@ -21026,9 +21026,8 @@ var _hoisted_3 = {
 var _hoisted_4 = {
   "class": "bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded"
 };
-var _hoisted_5 = ["onClick"];
 
-var _hoisted_6 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
+var _hoisted_5 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
   "class": "bg-blue-500 text-center p-2 rounded-b-lg text-gray-50 mt-2"
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("i", {
   "class": "fa-solid fa-circle-info mx-1"
@@ -21036,7 +21035,7 @@ var _hoisted_6 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementV
 /* HOISTED */
 );
 
-var _hoisted_7 = [_hoisted_6];
+var _hoisted_6 = [_hoisted_5];
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   var _component_QRCodeVue3 = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("QRCodeVue3");
 
@@ -21083,12 +21082,10 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
             , ["value"])]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("span", _hoisted_4, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(sl_qr_no), 1
             /* TEXT */
             ), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("a", {
-              onClick: function onClick($event) {
-                return _ctx.$emit('needHelp', sl_qr_no);
-              }
-            }, _hoisted_7, 8
-            /* PROPS */
-            , _hoisted_5)];
+              onClick: _cache[0] || (_cache[0] = function ($event) {
+                return _ctx.alert('hello');
+              })
+            }, _hoisted_6)];
           }),
           _: 2
           /* DYNAMIC */
