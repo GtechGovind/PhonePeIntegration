0b5c08f2f7e4544d32f2210168a1d6dbb47faa6d
Govind yadav <43997191+GtechGovind@users.noreply.github.com>
2022-03-13 22:44:22 +0530

    sX

--- public/js/app.js
+++ public/js/app.js
@@ -22040,7 +22040,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
         type: "button",
         "class": "text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white",
         "data-modal-toggle": "need-help"
-      }, _hoisted_6)]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_7, [_hoisted_8, _hoisted_9, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("button", {
+      }, _hoisted_6)]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_7, [_hoisted_8, _hoisted_9, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
         onClick: _cache[1] || (_cache[1] = function () {
           return $options.getRefundInfo && $options.getRefundInfo.apply($options, arguments);
         }),
--- resources/js/shared/Model/NeedHelpModel.vue
+++ resources/js/shared/Model/NeedHelpModel.vue
@@ -21,9 +21,9 @@
                         <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Please select the type of
                             help, you need ?
                         </h3>
-                        <button v-on:click="getRefundInfo" data-modal-toggle="need-help" class="m-1 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">
+                        <div v-on:click="getRefundInfo" data-modal-toggle="need-help" class="m-1 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">
                             Refund ticket !
-                        </button>
+                        </div>
                         <button v-on:click="$emit('unable-to-exit')" data-modal-toggle="need-help" type="button" class="m-1 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">
                             Unable to exit ?
                         </button>
