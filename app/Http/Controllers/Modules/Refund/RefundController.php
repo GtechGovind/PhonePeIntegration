0b70670b7071311130587250439b758e2cd5d9c7
Govind yadav <43997191+GtechGovind@users.noreply.github.com>
2022-03-14 22:16:08 +0530

    Update RefundController.php

--- app/Http/Controllers/Modules/Refund/RefundController.php
+++ app/Http/Controllers/Modules/Refund/RefundController.php
@@ -82,9 +82,7 @@ class RefundController extends Controller
             $refund_order_id,
             $response -> data -> details -> pass -> refundAmount
         );
-
-        dd($refundResponse);
-
+        
         if ($refundResponse -> success) {
 
             DB::table('refund_order')
