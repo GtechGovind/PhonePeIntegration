41f2db554227bccfdae4192563e0863a94881e22
Govind yadav <43997191+GtechGovind@users.noreply.github.com>
2022-03-16 00:13:06 +0530

    Update RefundController.php

--- app/Http/Controllers/Modules/Refund/RefundController.php
+++ app/Http/Controllers/Modules/Refund/RefundController.php
@@ -20,23 +20,6 @@ class RefundController extends Controller
         $api = new ApiController();
         $response = $api->getRefundInfo($order);
 
-        if ($order->pass_id == env('PASS_SV')) {
-
-            $lastOrder = DB::table('sale_order')
-                ->where('ms_qr_no', '=', $order->ms_qr_no)
-                ->orderBy('txn_date', 'desc')
-                ->first();
-
-            if (!is_null($lastOrder)) {
-                if (!$lastOrder->total_price - $response->data->details->pass->refundAmount > $lastOrder->total_price) {
-                    return response([
-                        'status' => false,
-                        'error' => 'Please spend ₹ ' . ($lastOrder->total_price - $response->data->details->pass->refundAmount) . ' to refund card !'
-                    ]);
-                }
-            }
-        }
-
         if ($response == null) return response([
             'status' => false,
             'error' => 'Please check your internet connection!'
@@ -47,14 +30,40 @@ class RefundController extends Controller
             'error' => $response->error
         ]);
 
+        $processing_fee = $response->data->details->pass->processingFee;
+        $processing_fee_amount = $response->data->details->pass->processingFeeAmount;
+        $refund_amount = $response->data->details->pass->refundAmount;
+        $pass_price = $response->data->details->pass->passPrice;
+
+        if ($order->pass_id == env('PASS_SV'))
+        {
+            $lastOrder = DB::table('sale_order')
+                ->where('ms_qr_no', '=', $order->ms_qr_no)
+                ->orderBy('txn_date', 'desc')
+                ->first();
+
+            if (!is_null($lastOrder))
+            {
+                $difference =  abs($lastOrder->total_price - $order->total_price);
+                if ($difference > $lastOrder->total_price)
+                {
+                    return response([
+                        'status' => false,
+                        'error' => 'Please spend ₹ ' . ($difference) . ' to refund pass !'
+                    ]);
+                }
+            }
+        }
+
+
         return response([
             'status' => true,
             'refund' => [
                 'order_id' => $order_id,
-                'processing_fee' => $response->data->details->pass->processingFee,
-                'processing_fee_amount' => $response->data->details->pass->processingFeeAmount,
-                'refund_amount' => $response->data->details->pass->refundAmount,
-                'pass_price' => $response->data->details->pass->passPrice
+                'processing_fee' => $processing_fee,
+                'processing_fee_amount' => $processing_fee_amount,
+                'refund_amount' => $refund_amount,
+                'pass_price' => $pass_price
             ]
         ]);
     }
@@ -75,14 +84,14 @@ class RefundController extends Controller
 
         if ($response->status == "BSE") return response([
             'status' => false,
-            'error' => $response -> error
+            'error' => $response->error
         ]);
 
-        if($order->product_id == env('PRODUCT_SJT') || $order->product_id == env('PRODUCT_RJT')) return $this -> sjtRjtRefund($order, $response);
-        else if ($order->product_id == env('PRODUCT_SV')) return $this -> svRefund($order, $response);
-        else if ($order->product_id == env('PRODUCT_TP')) return $this -> tpRefund($order, $response);
+        if ($order->product_id == env('PRODUCT_SJT') || $order->product_id == env('PRODUCT_RJT')) return $this->sjtRjtRefund($order, $response);
+        else if ($order->product_id == env('PRODUCT_SV')) return $this->svRefund($order, $response);
+        else if ($order->product_id == env('PRODUCT_TP')) return $this->tpRefund($order, $response);
 
-        return  response([
+        return response([
             'status' => false,
             'error' => 'Invalid product'
         ]);
@@ -92,43 +101,46 @@ class RefundController extends Controller
     private function sjtRjtRefund($order, $response)
     {
 
+        // GENERATING REFUND ORDER
         $refund_order_id = OrderUtility::genSaleOrderNumber($order->pass_id);
 
+        // CREATING REFUND ORDER
         DB::table('refund_order')
             ->insert([
-                'ref_or_no'         => $refund_order_id,
-                'sale_or_id'        => $order -> sale_or_id,
-                'pax_id'            => Auth::id(),
-                'unit'              => $order->unit,
-                'ref_amt'           => $response -> data -> details -> pass -> refundAmount,
-                'ref_chr'           => $response -> data -> details -> pass -> processingFee,
-                'ref_or_status'     => env('ISSUE'),
-                'txn_date'          => now()
+                'ref_or_no' => $refund_order_id,
+                'sale_or_id' => $order->sale_or_id,
+                'pax_id' => Auth::id(),
+                'unit' => $order->unit,
+                'ref_amt' => $response->data->details->pass->refundAmount,
+                'ref_chr' => $response->data->details->pass->processingFee,
+                'ref_or_status' => env('ISSUE'),
+                'txn_date' => now()
             ]);
 
+        // REFUNDING PHONEPE
         $phonepe = new PhonePeRefundController();
+        $refundResponse = $phonepe->init($order, $refund_order_id, $response->data->details->pass->refundAmount);
 
-        $refundResponse = $phonepe -> init(
-            $order,
-            $refund_order_id,
-            $response -> data -> details -> pass -> refundAmount
-        );
-
-        if (!$refundResponse -> success) {
+        // IF PHONEPE REFUND FAILED
+        if (!$refundResponse->success) {
             return response([
-                'status' => true,
-                'message' => 'Order refunded Successfully.'
+                'status' => false,
+                'error' => 'failed to refund order, try again or contact phonepe!'
             ]);
         }
 
+        // UPDATING REFUND ORDER
         DB::table('refund_order')
-            ->where('sale_or_id', '=', $order -> sale_or_id)
+            ->where('sale_or_id', '=', $order->sale_or_id)
             ->update([
-                'pg_txn_no' => $refundResponse -> data -> providerReferenceId
+                'pg_txn_no' => $refundResponse->data->providerReferenceId
             ]);
 
-        $refundApiResponse = $api -> refundTicket($response, $refund_order_id);
+        // REFUNDING MMOPL
+        $api = new ApiController();
+        $refundApiResponse = $api->refundTicket($response, $refund_order_id);
 
+        // MMOPL ERRORS
         if ($refundApiResponse == null) {
             return response([
                 'status' => false,
@@ -139,12 +151,13 @@ class RefundController extends Controller
         if ($refundApiResponse->status == "BSE") {
             return response([
                 'status' => false,
-                'error' => $response -> error
+                'error' => $response->error
             ]);
         }
 
+        // UPDATING OLD ORDER
         DB::table('sale_order')
-            ->where('sale_or_no', '=', $order_id)
+            ->where('sale_or_no', '=', $order->order_id)
             ->update([
                 'sale_or_status' => env('ORDER_REFUNDED')
             ]);
@@ -168,9 +181,8 @@ class RefundController extends Controller
 
         if ($response->status == "BSE") return response([
             'status' => false,
-            'error' => $response -> error
+            'error' => $response->error
         ]);
-
 
 
     }
