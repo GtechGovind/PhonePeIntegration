b7f3435e85143b753fddbabd557fe99bb9308f75
Govind yadav <43997191+GtechGovind@users.noreply.github.com>
2022-03-15 15:24:13 +0530

    s

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,7 +45,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            BasicAuth::class
+
         ],
     ];
 
@@ -66,5 +66,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        BasicAuth::class
     ];
 }
