0365bb74b14c5e37f026408772eb9b1473156718
Govind yadav <43997191+GtechGovind@users.noreply.github.com>
2022-03-15 15:30:19 +0530

    s

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Http;
 
-use App\Http\Middleware\BasicAuth;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -66,6 +65,5 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        BasicAuth::class
     ];
 }
--- app/Http/Middleware/BasicAuth.php
+++ app/Http/Middleware/BasicAuth.php
@@ -7,7 +7,13 @@ use Illuminate\Http\Request;
 
 class BasicAuth
 {
-
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
     public function handle(Request $request, Closure $next)
     {
         $AUTH_USER = 'admin';
