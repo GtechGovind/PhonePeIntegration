30ca86ef18bdcf74dab72f8f3658bbf0bb5da62a
Govind yadav <43997191+GtechGovind@users.noreply.github.com>
2022-03-16 15:16:18 +0530

    sd

--- resources/js/pages/Modules/Ticket/Components/OrderHelper.vue
+++ resources/js/pages/Modules/Ticket/Components/OrderHelper.vue
@@ -92,7 +92,7 @@
                 <div class="grid grid-cols-2">
                     <div class="text-gray-600">Journey Type</div>
                     <div class="text-right font-bold text-gray-600">
-                        {{ ticket.pass_id === 10 ? "Single Journey" : "Return Journey" }}
+                        {{ ticket.pass_id === "10" ? "Single Journey" : "Return Journey" }}
                     </div>
                     <div class="text-gray-600">Passengers</div>
                     <div class="text-right font-bold text-gray-600">
@@ -144,7 +144,7 @@ export default {
                 destination_id: '',
                 quantity: 1,
                 fare: 0,
-                pass_id: 10
+                pass_id: "10"
             },
             errors: {
                 source_id: null,
