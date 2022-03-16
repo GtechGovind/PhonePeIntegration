ec1bf65e8f10e63ea49fb083bb74020a64295a3b
Govind yadav <43997191+GtechGovind@users.noreply.github.com>
2022-03-17 01:00:26 +0530

    gh

--- public/js/app.js
+++ public/js/app.js
@@ -24141,104 +24141,103 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 
-var _hoisted_1 = {
-  "class": "m-2 bg-blue-500 rounded-xl relative text-white shadow"
-};
-var _hoisted_2 = ["src"];
-var _hoisted_3 = {
+var _hoisted_1 = ["src"];
+var _hoisted_2 = {
   "class": "w-full px-8 absolute top-8"
 };
-var _hoisted_4 = {
+var _hoisted_3 = {
   "class": "flex justify-between"
 };
 
-var _hoisted_5 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", {
+var _hoisted_4 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", {
   "class": "font-light"
 }, "Name", -1
 /* HOISTED */
 );
 
-var _hoisted_6 = {
+var _hoisted_5 = {
   "class": "tracking-widest font-bold"
 };
-var _hoisted_7 = ["src"];
-var _hoisted_8 = {
-  "class": "pt-4"
+var _hoisted_6 = ["src"];
+var _hoisted_7 = {
+  "class": "mt-1"
 };
 
-var _hoisted_9 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", {
+var _hoisted_8 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", {
   "class": "font-light"
 }, "Master Number", -1
 /* HOISTED */
 );
 
-var _hoisted_10 = {
+var _hoisted_9 = {
   "class": "tracking-more-wider font-bold"
 };
-var _hoisted_11 = {
+var _hoisted_10 = {
   key: 0,
   "class": "flex justify-between"
 };
 
-var _hoisted_12 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", {
+var _hoisted_11 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", {
   "class": "font-light"
 }, "Source", -1
 /* HOISTED */
 );
 
-var _hoisted_13 = {
+var _hoisted_12 = {
   "class": "tracking-widest font-bold"
 };
 
-var _hoisted_14 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", {
+var _hoisted_13 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", {
   "class": "font-light"
 }, "Destination", -1
 /* HOISTED */
 );
 
-var _hoisted_15 = {
+var _hoisted_14 = {
   "class": "tracking-widest font-bold"
 };
-var _hoisted_16 = {
-  "class": "pt-2"
+var _hoisted_15 = {
+  "class": "mt-1"
 };
-var _hoisted_17 = {
+var _hoisted_16 = {
   "class": "flex justify-between"
 };
-var _hoisted_18 = {
+var _hoisted_17 = {
   "class": "font-light text-xs"
 };
-var _hoisted_19 = {
+var _hoisted_18 = {
   "class": "tracking-wider text-sm font-bold"
 };
 
-var _hoisted_20 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", {
+var _hoisted_19 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", {
   "class": "font-light text-xs"
 }, "Expiry", -1
 /* HOISTED */
 );
 
-var _hoisted_21 = {
+var _hoisted_20 = {
   "class": "tracking-wider text-sm font-bold"
 };
-var _hoisted_22 = {
+var _hoisted_21 = {
   key: 0,
   "class": "flex flex-row-reverse"
 };
-var _hoisted_23 = {
+var _hoisted_22 = {
   key: 1,
   "class": "flex flex-row-reverse"
 };
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   var _component_ReloadButton = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("ReloadButton");
 
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_1, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("img", {
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", {
+    "class": (0,vue__WEBPACK_IMPORTED_MODULE_0__.normalizeClass)([$props.isSv ? 'h-50' : 'h-64', "m-2 bg-blue-500 rounded-xl relative text-white shadow"])
+  }, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("img", {
     "class": "relative object-cover w-full h-full rounded-xl",
     src: $data.card_img,
     alt: ""
   }, null, 8
   /* PROPS */
-  , _hoisted_2), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_3, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("USER NAME & COMPANY LOGO"), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_4, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, [_hoisted_5, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_6, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props.user.pax_name), 1
+  , _hoisted_1), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_2, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("USER NAME & COMPANY LOGO"), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_3, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, [_hoisted_4, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_5, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props.user.pax_name), 1
   /* TEXT */
   )]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("img", {
     "class": "w-28 h-auto",
@@ -24246,19 +24245,19 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
     alt: ""
   }, null, 8
   /* PROPS */
-  , _hoisted_7)]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("CARD NUMBER"), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_8, [_hoisted_9, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_10, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props.passDetails.ms_qr_no), 1
+  , _hoisted_6)]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("CARD NUMBER"), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_7, [_hoisted_8, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_9, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props.passDetails.ms_qr_no), 1
   /* TEXT */
-  )]), !$props.isSv ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_11, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, [_hoisted_12, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_13, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props.passDetails.source), 1
+  )]), !$props.isSv ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_10, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, [_hoisted_11, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_12, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props.passDetails.source), 1
   /* TEXT */
-  )]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, [_hoisted_14, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_15, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props.passDetails.destination), 1
+  )]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, [_hoisted_13, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_14, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props.passDetails.destination), 1
   /* TEXT */
-  )])])) : (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("v-if", true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_16, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_17, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("BALANCE"), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_18, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props.isSv ? 'Balance' : 'Balance Trips'), 1
+  )])])) : (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("v-if", true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_15, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_16, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("BALANCE"), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_17, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props.isSv ? 'Balance' : 'Balance Trips'), 1
   /* TEXT */
-  ), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_19, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props.isSv ? $props.balance === 0 ? 'fetching ..' : '₹ ' + $props.balance : $props.balance === 0 ? 'fetching ..' : $props.balance + ' Trips'), 1
+  ), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_18, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props.isSv ? $props.balance === 0 ? 'fetching ..' : '₹ ' + $props.balance : $props.balance === 0 ? 'fetching ..' : $props.balance + ' Trips'), 1
   /* TEXT */
-  )]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("EXP DATE"), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, [_hoisted_20, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_21, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(new Date($props.passDetails.ms_qr_exp).toLocaleDateString()), 1
+  )]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("EXP DATE"), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, [_hoisted_19, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_20, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(new Date($props.passDetails.ms_qr_exp).toLocaleDateString()), 1
   /* TEXT */
-  )]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("RELOAD"), $props.isSv ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_22, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_ReloadButton, {
+  )]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("RELOAD"), $props.isSv ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_21, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_ReloadButton, {
     type: 'button',
     "is-disabled": $data.isLoading,
     "is-loading": $data.isLoading,
@@ -24266,7 +24265,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
     onClick: $options.svReloadStatus
   }, null, 8
   /* PROPS */
-  , ["is-disabled", "is-loading", "onClick"])])) : (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("v-if", true), !$props.isSv ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_23, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_ReloadButton, {
+  , ["is-disabled", "is-loading", "onClick"])])) : (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("v-if", true), !$props.isSv ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_22, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_ReloadButton, {
     type: 'button',
     "is-disabled": $data.isLoading,
     "is-loading": $data.isLoading,
@@ -24274,7 +24273,9 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
     onClick: $options.tpReloadStatus
   }, null, 8
   /* PROPS */
-  , ["is-disabled", "is-loading", "onClick"])])) : (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("v-if", true)])])])]);
+  , ["is-disabled", "is-loading", "onClick"])])) : (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("v-if", true)])])])], 2
+  /* CLASS */
+  );
 }
 
 /***/ }),
--- resources/js/shared/Card.vue
+++ resources/js/shared/Card.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div class="m-2 bg-blue-500 rounded-xl relative text-white shadow">
+    <div :class="isSv ? 'h-50' : 'h-64'" class="m-2 bg-blue-500 rounded-xl relative text-white shadow">
         <img class="relative object-cover w-full h-full rounded-xl" :src="card_img" alt="">
         <div class="w-full px-8 absolute top-8">
 
@@ -19,36 +19,32 @@
             </div>
 
             <!--CARD NUMBER-->
-            <div class="pt-4">
+            <div class="mt-1">
                 <p class="font-light">Master Number</p>
                 <p class="tracking-more-wider font-bold">
                     {{ passDetails.ms_qr_no }}
                 </p>
             </div>
 
-            <template class="pt-4" v-if="!isSv">
+            <template class="mt-1" v-if="!isSv">
                 <div class="flex justify-between">
-
                     <div>
                         <p class="font-light">Source</p>
                         <p class="tracking-widest font-bold">
                             {{ passDetails.source }}
                         </p>
                     </div>
-
                     <div>
                         <p class="font-light">Destination</p>
                         <p class="tracking-widest font-bold">
                             {{ passDetails.destination }}
                         </p>
                     </div>
-
                 </div>
             </template>
 
-            <div class="pt-2">
+            <div class="mt-1">
                 <div class="flex justify-between">
-
                     <!--BALANCE-->
                     <div>
                         <p class="font-light text-xs">{{ isSv ? 'Balance' : 'Balance Trips' }}</p>
@@ -61,7 +57,6 @@
                             }}
                         </p>
                     </div>
-
                     <!--EXP DATE-->
                     <div>
                         <p class="font-light text-xs">Expiry</p>
@@ -93,6 +88,7 @@
 
                 </div>
             </div>
+
         </div>
     </div>
 
