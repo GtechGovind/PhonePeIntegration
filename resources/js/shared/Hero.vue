ba3f92fa507573a38045a3ba6b467e9264341e52
Govind yadav <43997191+GtechGovind@users.noreply.github.com>
2022-03-21 13:04:17 +0530

    Update Hero.vue

--- resources/js/shared/Hero.vue
+++ resources/js/shared/Hero.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="relative rounded-lg m-2 bg-white">
         <div class="grid w-full content-center p-7" >
-            <img :src="logo" alt="..." class="m-auto">
+            <img :src="logo" alt="..." class="h-24 mx-auto">
         </div>
     </div>
 </template>
