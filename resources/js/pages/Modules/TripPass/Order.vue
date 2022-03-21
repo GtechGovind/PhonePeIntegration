df423588e0c1376af9084a5d2a516df44ab4d302
Govind yadav <43997191+GtechGovind@users.noreply.github.com>
2022-03-21 14:56:41 +0530

    sd

--- resources/js/pages/Modules/TripPass/Order.vue
+++ resources/js/pages/Modules/TripPass/Order.vue
@@ -160,10 +160,12 @@ export default {
 
         isValid: function () {
 
-            if(this.tripPass.source_id === this.tripPass.destination_id) {
+            if(this.tripPass.fare === 0) {
+
                 this.isDisabled = true
                 this.errors.source_id = 'Source & destination can\'t be same !'
                 this.errors.destination_id = 'Source & destination can\'t be same !'
+
             } else {
                 this.isDisabled = false
                 return  true
